e11e1c751c9fe22fe9720c55309afda564c800e9
sekumpulananakupi <sekumpulananakupi@gmail.com>
2026-06-13 21:16:40 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menuToggle");
   const navMenu = document.getElementById("navMenu");
 
-  if (menuToggle && navMenu) {
-    menuToggle.addEventListener("click", () => {
-      navMenu.classList.toggle("show");
-    });
-  }
+  if (!menuToggle || !navMenu) return;
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
 });
